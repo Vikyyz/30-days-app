25cbb80d3bfb9cad878f98611006b1ca17a400de
Vikyyz <ilieviky13@gmail.com>
2025-08-12 19:26:54 +0300

    wip header

--- app/src/main/java/com/example/a30daysapp/MainActivity.kt
+++ app/src/main/java/com/example/a30daysapp/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,11 +15,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -24,7 +29,9 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxValue
@@ -35,12 +42,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a30daysapp.data.DataSource
@@ -68,7 +77,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TipsCard(tip: Tip, modifier: Modifier = Modifier) {
+fun TipsCard(tip: Tip, index: Int, modifier: Modifier = Modifier) {
     var completed by remember { mutableStateOf(false) }
     Card(modifier = modifier.padding(8.dp), elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
         val dismissState = rememberSwipeToDismissBoxState(
@@ -81,14 +90,20 @@ fun TipsCard(tip: Tip, modifier: Modifier = Modifier) {
                 }
             }
         )
+        Text (
+            text = "Day ${index + 1}",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(10.dp)
+        )
         SwipeToDismissBox (
             state = dismissState,
             enableDismissFromEndToStart = true,
+            enableDismissFromStartToEnd = false,
             backgroundContent = {
                 Box {
                     Icon(
                         imageVector = Icons.Default.Delete,
-                        contentDescription = "Delete tip"
+                        contentDescription = "Delete tip",
                     )
                 }
             }
@@ -101,7 +116,7 @@ fun TipsCard(tip: Tip, modifier: Modifier = Modifier) {
                         .padding(16.dp),
                     contentAlignment = androidx.compose.ui.Alignment.Center
                 ) {
-                    Text(text = "You have completed this day's task")
+                    Text(text = "You have completed day's ${index + 1} task.")
                 }
             } else {
                 Column {
@@ -112,7 +127,6 @@ fun TipsCard(tip: Tip, modifier: Modifier = Modifier) {
                             .fillMaxWidth()
                             .height(194.dp),
                         contentScale = ContentScale.Crop
-
                     )
                     Text (
                         text = stringResource(tip.stringResourceId),
@@ -128,7 +142,7 @@ fun TipsCard(tip: Tip, modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun TipsCardPreview () {
-    TipsCard(Tip(R.string.Tips1, R.drawable.image1))
+    TipsCard(Tip(R.string.Tips1, R.drawable.image1), index = 0)
 }
 
 @Composable
@@ -141,28 +155,79 @@ private fun TipsList(tipsList: List<Tip>, modifier: Modifier = Modifier) {
         item (span = { GridItemSpan(this.maxLineSpan) }) {
             ArtBlockDetailsHeaderItem()
         }
-        items(tipsList) { tip ->
-            TipsCard(tip)
+
+        itemsIndexed (tipsList) { index, tip ->
+            TipsCard(
+                tip = tip,
+                index = index,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
         }
     }
 }
 @Composable
 fun ArtBlockDetailsHeaderItem(modifier: Modifier = Modifier) {
-    Card(modifier = Modifier.padding(8.dp)) {
-        Column {
-            Text (
-                text = "30 Days to combat Art Block",
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(16.dp)
+//    Header - Entire header
+    Column(
+        modifier = modifier.padding(5.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    )
+//    Header - Image and title
+    {
+        Row(
+            modifier = modifier
+                .padding(5.dp)
+                .padding(top = 15.dp),
+            verticalAlignment = Alignment.Top
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.header1),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(110.dp),
+                contentScale = ContentScale.Crop
             )
+
+            Column(modifier = Modifier.padding(start = 8.dp)) {
+                Text(
+                    text = "30 Days to combat Art Block",
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                LinearProgressIndicator(
+                    progress = {
+                        0.33f
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    color = MaterialTheme.colorScheme.primary,
+                    trackColor = ProgressIndicatorDefaults.linearTrackColor,
+                    strokeCap = ProgressIndicatorDefaults.LinearStrokeCap,
+                )
+            }
+        }
+//        Header - Description
+        var expanded by remember { mutableStateOf(false) }
+        Column {
             Text(
-                text = "Art Block",
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(16.dp)
+                text = stringResource(R.string.Description),
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = if (expanded) Int.MAX_VALUE else 3,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Text(
+                text = if (expanded) "See less" else "See more",
+                style = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.primary),
+                modifier = Modifier
+                    .animateContentSize()
+                    .clickable { expanded = !expanded }
             )
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
