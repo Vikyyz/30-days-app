a8049632a036ed0223abdac22de02814d5b6829a
Vikyyz <ilieviky13@gmail.com>
2025-08-12 11:55:28 +0300

    feature added: basic scrollable lazy column

--- app/src/main/java/com/example/a30daysapp/MainActivity.kt
+++ app/src/main/java/com/example/a30daysapp/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30daysapp.data.DataSource
+import com.example.a30daysapp.model.Tip
 import com.example.a30daysapp.ui.theme._30DaysAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +33,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             _30DaysAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Viky",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold (
+                    content =  {
+                        TipsList(
+                            tipsList = DataSource().loadTips(),
+                            modifier = Modifier.padding(it)
+                        )
+                    }
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun TipsCard(tip: Tip, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.padding(8.dp), elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+        Column {
+            Image(
+                painter = painterResource(tip.imageResourceId),
+                contentDescription = stringResource(tip.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+
+            )
+            Text (
+                text = stringResource(tip.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall // aici trebuie sa fie h6
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TipsCardPreview () {
+    TipsCard(Tip(R.string.Tips1, R.drawable.image1))
+}
+
+@Composable
+private fun TipsList(tipsList: List<Tip>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(tipsList) { tip ->
+            TipsCard(tip)
         }
     }
 }
--- app/src/main/java/com/example/a30daysapp/data/DataSource.kt
+++ app/src/main/java/com/example/a30daysapp/data/DataSource.kt
@@ -1,4 +1,20 @@
 package com.example.a30daysapp.data
 
-class DataSource {
+import com.example.a30daysapp.R
+import com.example.a30daysapp.model.Tip
+
+class DataSource () {
+    fun loadTips () : List<Tip> {
+        return listOf<Tip>(
+            Tip(R.string.Tips1, R.drawable.image1),
+            Tip(R.string.Tips2, R.drawable.image2),
+            Tip(R.string.Tips3, R.drawable.image3),
+            Tip(R.string.Tips4, R.drawable.image4),
+            Tip(R.string.Tips5, R.drawable.image5),
+            Tip(R.string.Tips6, R.drawable.image6),
+            Tip(R.string.Tips7, R.drawable.image7),
+            Tip(R.string.Tips8, R.drawable.image8),
+            Tip(R.string.Tips9, R.drawable.image9),
+            Tip(R.string.Tips10, R.drawable.image10))
+    }
 }
