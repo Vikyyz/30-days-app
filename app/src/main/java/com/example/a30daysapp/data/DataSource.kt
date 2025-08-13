1928e044bf3db3924aec353981bdb51b22d8c817
Vikyyz <ilieviky13@gmail.com>
2025-08-13 10:04:58 +0300

    added new parameter to a tip class for persistent day tracking

--- app/src/main/java/com/example/a30daysapp/data/DataSource.kt
+++ app/src/main/java/com/example/a30daysapp/data/DataSource.kt
@@ -4,17 +4,22 @@ import com.example.a30daysapp.R
 import com.example.a30daysapp.model.Tip
 
 class DataSource () {
-    fun loadTips () : List<Tip> {
-        return listOf<Tip>(
-            Tip(R.string.Tips1, R.drawable.image1),
-            Tip(R.string.Tips2, R.drawable.image2),
-            Tip(R.string.Tips3, R.drawable.image3),
-            Tip(R.string.Tips4, R.drawable.image4),
-            Tip(R.string.Tips5, R.drawable.image5),
-            Tip(R.string.Tips6, R.drawable.image6),
-            Tip(R.string.Tips7, R.drawable.image7),
-            Tip(R.string.Tips8, R.drawable.image8),
-            Tip(R.string.Tips9, R.drawable.image9),
-            Tip(R.string.Tips10, R.drawable.image10))
+    fun loadTips(): List<Tip> {
+        val tips = listOf(
+            Tip(R.string.Tips1, R.drawable.image1, 0),
+            Tip(R.string.Tips2, R.drawable.image2, 0),
+            Tip(R.string.Tips3, R.drawable.image3, 0),
+            Tip(R.string.Tips4, R.drawable.image4, 0),
+            Tip(R.string.Tips5, R.drawable.image5, 0),
+            Tip(R.string.Tips6, R.drawable.image6, 0),
+            Tip(R.string.Tips7, R.drawable.image7, 0),
+            Tip(R.string.Tips8, R.drawable.image8, 0),
+            Tip(R.string.Tips9, R.drawable.image9, 0),
+            Tip(R.string.Tips10, R.drawable.image10, 0)
+        )
+
+        return tips.mapIndexed { index, tip ->
+            tip.copy(day = index)
+        }
     }
 }
